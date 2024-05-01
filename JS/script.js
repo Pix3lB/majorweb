ffa1759db4ef1df82e2da7ec960d5b5ef5198762
Pixel <yusufaffanazmi@gmail.com>
2024-05-01 21:25:58 +0530

    removed rsa,cleaned up css for index,stenography & caesar

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 var i = 0;
 var txt = 'INZHS4DUN5'; /* The text */
-var speed = 500; /* The speed/duration of the effect in milliseconds */
+var speed = 150; /* The speed/duration of the effect in milliseconds */
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("a").innerHTML += txt.charAt(i);
+    document.getElementById("shown").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
